4d77efead32563f39f5cb7bcc2fe6549bcbe87b5
Erik Jan <40488822+evroom@users.noreply.github.com>
2024-11-11 18:51:47 +0100

    Updated

--- HoymilesWifi.sh
+++ HoymilesWifi.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Run the Python script to generate the HTML file
-python hoymiles_data.py
-
-   
-
+# Provide the DTU IP address
+python hoymiles_data.py --dtu_ip_address <DTU-IP-ADDRESS> --debug
