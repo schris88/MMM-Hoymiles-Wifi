64fdc37644d800e80494ae2e95b4b49fa161b6d1
christian.stengel@arorian.com <30658603+schris88@users.noreply.github.com>
2024-11-27 17:32:12 +0100

    Update HoymilesWifi.sh

--- HoymilesWifi.sh
+++ HoymilesWifi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run the Python script to generate the HTML file
 # Provide the DTU IP address
-python hoymiles_data.py --dtu_ip_address <DTU-IP-ADDRESS> --max 300
+python hoymiles_data.py --dtu_ip_address <DTU-IP-ADDRESS> --max <MAX WATTAGE>
